4bb333f78a143cd008e0f8b254c7545876419e9d
zhaoyiyang <xmtest1@126.com>
2015-08-19 17:44:14 +0800

    test

---
+++ src/web.go
@@ -0,0 +1,12 @@
+package main
+
+import "net/http"
+
+func main() {
+	http.HandleFunc("/", hello)
+	http.ListenAndServe(":8081", nil)
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("go web!"))
+}
